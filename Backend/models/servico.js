f146fcfff9569b1a24fb6aae64a5212bb7a87ef7
Guilherme Matos <guimatos.com@gmail.com>
2022-09-14 22:57:15 -0300

    comeco conexao BD

---
+++ Backend/models/servico.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+const Usuario = require('./usuario')
+
+const Servico = database.define('servico', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncremente: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    valor: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+
+    descricao: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+Usuario.hasMany(Usuario,{
+    foreignKey: 'cpf'
+})
+
+module.exports = Servico;
