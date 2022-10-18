142d5c4ad5826e47ff7f3bc6e4d5c866bc562744
RenanGAS <renansakaoki@gmail.com>
2022-10-18 12:08:09 -0400

    chore: make the debug banner disappear

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -52,6 +52,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Freela App',
         home: SafeArea(
           child: MyHomePage(),
