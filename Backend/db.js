25792a7a4225695835a58d00f4d6d212e5c5697a
RenanGAS <renansakaoki@gmail.com>
2022-11-13 11:19:34 -0400

    chore: move db.js to config/db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,18 +1,20 @@
-const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
-require('dotenv').config('./.env');
-console.log(process.env);
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: "mysql",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT)
-});
+// Movido para config/db.js
 
-try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+// const Sequelize = require('sequelize');
+// const dotenv = require('dotenv');
+// require('dotenv').config('./.env');
+// console.log(process.env);
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     dialect: "mysql",
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT)
+// });
 
-module.exports = sequelize;
+// try {
+//     sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+
+// module.exports = sequelize;
