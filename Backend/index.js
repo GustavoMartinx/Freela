5957c3f34594c10863bde9ea50c08cf02ab50972
RenanGAS <renansakaoki@gmail.com>
2022-11-13 11:19:06 -0400

    chore: construct the app in index.js and move the class functions to the DAOs folder

--- Backend/index.js
+++ Backend/index.js
@@ -1,121 +1,139 @@
+// Instanciação da aplicação
+
+import express from 'express';
+import cors from 'cors';
+import database from './config/db.js';
+import routes from './routes/routes.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 (async () => {
-    //INICIALIZACAO SERVER
-    const http = require('http');
-    const app = require('./server');
-    const port = process.env.PORT || 3000;
-    const server = http.createServer(app)
-    
-    const database = require('./db');
-    const Disponibilidade = require('./models/disponibilidade');
-    const Endereco = require('./models/endereco');
-    const Servico = require('./models/servico');
-    const Usuario = require('./models/usuario');
-    const Categoria = require('./models/categoria')
-    
-    class USUARIO{
-        cadastrarUsuario(CPF, NOME, DESCRICAO,AVALIACAO,SENHA){
-            Usuario.create({
-                cpf: CPF,
-                nome: NOME,
-                descricao: DESCRICAO,
-                avaliacao: AVALIACAO,
-                senha: SENHA
-            })
-            console.log("cadastrado "+ CPF)
-        }
-        
-        procurarUsuario(CPF){
-            return Usuario.findByPk(CPF)
-        }
-        
-        deletarUsuario(CPF){
-            Usuario.destroy({
-                where: {
-                    cpf: CPF
-                }
-            })
-        }
-    }
-    
-    class SERVICO{
-        gerarServico(ID, VALOR, DESCRICAO, CPF_GERA, CPF_ACEITA){
-            Servico.create({
-                id: ID,
-                valor: VALOR,
-                descricao: DESCRICAO,
-                geracpf: CPF_GERA,
-                aceitacpf: CPF_ACEITA
-            })
-        }
-        
-        aceitaServico(ID, VALOR, DESCRICAO, CPF_GERA, CPF_ACEITA){
-            Servico.create({
-                id: ID,
-                valor: VALOR,
-                descricao: DESCRICAO,
-                geracpf: CPF_GERA,
-                aceitacpf: CPF_ACEITA
-            })
-        }
-        
-        servicosAceitos(CPF_ACEITA){
-            return Servico.findAll({
-                where: {
-                    cpf: CPF_ACEITA
-                }
-            });
-        }
-        
-        servicosGerados(CPF_ACEITA){
-            return Servico.findAll({
-                where: {
-                    cpf: CPF_ACEITA
-                }
-            });
-        }
-    }
-    
-    class DISPONIBILIDADE{
-        adicionarDisponibilidade(ID ,SERVICO_ID, DATA, SEMANAL, HORARIO_INICIO, HORARIO_FIM){
-            Disponibilidade.create({
-                id: ID,
-                servicoID: SERVICO_ID, 
-                data: DATA,
-                semanal: SEMANAL,
-                horario_inicio: HORARIO_INICIO,
-                horario_fim: HORARIO_FIM
-            })
-        }
-        
-        procurarDisponibilidade(SERVICO_ID){
-            return Disponibilidade.findAll({
-                where: {
-                    servicoID: SERVICO_ID,
-                }
-            })
-        }
-    }
-    
-    class CATEGORIA{
-        cadastrarCategoria(ID, NOME){
-            Categoria.create({
-                id: ID,
-                nome: NOME
-            })
-        }
-        
-        pesquisarCategorias(){
-            
-        }
-    }
-    
     await database.sync();
-    const user = new USUARIO
-    //user.deletarUsuario('464464464');
-    //user.cadastrarUsuario('464464464', 'Cleber', 'Cliente bom', 5, '911234');
-    
-
-    //SERVER SENDO LIGADO
-    server.listen(port)
 })();
+
+app.use('/', routes);
+
+export default app;
+
+
+// Inicialização do server em server.js
+
+// const http = require('http');
+// const app = require('./server');
+// const port = process.env.PORT || 3000;
+// const server = http.createServer(app)
+
+
+// Funções de cada classe em DAOs/classeDAO.js
+
+// const database = require('./db');
+// const Disponibilidade = require('./models/disponibilidade');
+// const Endereco = require('./models/endereco');
+// const Servico = require('./models/servico');
+// const Usuario = require('./models/usuario');
+// const Categoria = require('./models/categoria')
+
+// class USUARIO {
+//     cadastrarUsuario(CPF, NOME, DESCRICAO, AVALIACAO, SENHA) {
+//         Usuario.create({
+//             cpf: CPF,
+//             nome: NOME,
+//             descricao: DESCRICAO,
+//             avaliacao: AVALIACAO,
+//             senha: SENHA
+//         })
+//         console.log("cadastrado " + CPF)
+//     }
+
+//     procurarUsuario(CPF) {
+//         return Usuario.findByPk(CPF)
+//     }
+
+//     deletarUsuario(CPF) {
+//         Usuario.destroy({
+//             where: {
+//                 cpf: CPF
+//             }
+//         })
+//     }
+// }
+
+// class SERVICO {
+//     gerarServico(ID, VALOR, DESCRICAO, CPF_GERA, CPF_ACEITA) {
+//         Servico.create({
+//             id: ID,
+//             valor: VALOR,
+//             descricao: DESCRICAO,
+//             geracpf: CPF_GERA,
+//             aceitacpf: CPF_ACEITA
+//         })
+//     }
+
+//     aceitaServico(ID, VALOR, DESCRICAO, CPF_GERA, CPF_ACEITA) {
+//         Servico.create({
+//             id: ID,
+//             valor: VALOR,
+//             descricao: DESCRICAO,
+//             geracpf: CPF_GERA,
+//             aceitacpf: CPF_ACEITA
+//         })
+//     }
+
+//     servicosAceitos(CPF_ACEITA) {
+//         return Servico.findAll({
+//             where: {
+//                 cpf: CPF_ACEITA
+//             }
+//         });
+//     }
+
+//     servicosGerados(CPF_ACEITA) {
+//         return Servico.findAll({
+//             where: {
+//                 cpf: CPF_ACEITA
+//             }
+//         });
+//     }
+// }
+
+// class DISPONIBILIDADE {
+//     adicionarDisponibilidade(ID, SERVICO_ID, DATA, SEMANAL, HORARIO_INICIO, HORARIO_FIM) {
+//         Disponibilidade.create({
+//             id: ID,
+//             servicoID: SERVICO_ID,
+//             data: DATA,
+//             semanal: SEMANAL,
+//             horario_inicio: HORARIO_INICIO,
+//             horario_fim: HORARIO_FIM
+//         })
+//     }
+
+//     procurarDisponibilidade(SERVICO_ID) {
+//         return Disponibilidade.findAll({
+//             where: {
+//                 servicoID: SERVICO_ID,
+//             }
+//         })
+//     }
+// }
+
+// class CATEGORIA {
+//     cadastrarCategoria(ID, NOME) {
+//         Categoria.create({
+//             id: ID,
+//             nome: NOME
+//         })
+//     }
+
+//     pesquisarCategorias() {
+
+//     }
+// }
+
+//const user = new USUARIO
+//user.deletarUsuario('464464464');
+//user.cadastrarUsuario('464464464', 'Cleber', 'Cliente bom', 5, '911234');
+
+// server.listen(port)
