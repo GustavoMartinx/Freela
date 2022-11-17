dd2593cb254a6b84622443deb8b4311845632afd
Fgarm <guilhermear.maturana@outlook.com>
2022-11-17 11:24:24 -0300

    Implemented login and basic auth capabilities

--- Backend/DAOs/usuarioDAO.js
+++ Backend/DAOs/usuarioDAO.js
@@ -1,15 +1,33 @@
 import Usuario from '../models/usuario.js';
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 class DAOUsuario {
-    async store(req, res) {
+    async cadastrar(req, res) {
 
         var dados = req.body;
-
+        // criptografar a senha do usuário antes de guardar no banco de dados
+        dados.senha = bcrypt.hash(dados.senha, 10)
+        //cria um usuário e insere no BD
         const user = await Usuario.create(dados, (err) => {
             if (err) return res.status(400).json({        
                 error: true,
                 code: 101,
                 message: "Error: Usuário não foi cadastrado com sucesso!"
+            });
+            // cria um json web token para manter logado
+            // é necessário passar esse token sempre q fizer algo 
+            const maxAge = 3 * 60 * 60;
+            const token = jwt.sign(
+                { id: user.cpf, nome},
+                    jwtSecret,
+                {
+                    expiresIn: maxAge, // 3hrs in sec
+                }
+            );
+            res.cookie("jwt", token, {
+                httpOnly: true,
+                maxAge: maxAge * 1000, // 3hrs in ms
             });
             // return res.status(200).json({
             //     error: false,
@@ -40,9 +58,45 @@ class DAOUsuario {
             });
         });
     }
+    async login(req, res, next){
+            try {
+            // procura o usuário que tentou logar no banco de dados
+            const user = await Usuario.findOne({ where: {id: req.params.id} })
+            bcrypt.compare(req.senha, user.senha).then(function (result) {
+            //cria um json web token pra ele
+            // é necessário passar esse token sempre q fizer algo 
+              if (result) {
+                const maxAge = 3 * 60 * 60;
+                const token = jwt.sign(
+                  {id: user.cpf, nome},
+                  jwtSecret,
+                  {
+                    expiresIn: maxAge, // 3hrs in sec
+                  }
+                );
+                res.cookie("jwt", token, {
+                  httpOnly: true,
+                  maxAge: maxAge * 1000, // 3hrs in ms
+                });
+                res.status(201).json({
+                  message: "User successfully Logged in"
+                });
+              } else {
+                res.status(400).json({ message: "Login not succesful" });
+              }
+            });
+          } catch (error) {
+          res.status(400).json({
+            message: "An error occurred",
+            error: error.message,
+          });
+        }
+        };
 
     async delete(req, res) {
 
+        // de preferencia fazer com que um usuário tenha que estar logado para isso funcionar
+        // usar o userAuth do config/auth.js
         const user = await Usuario.deleteOne({ id: req.params.id }, (err) => {
             if (err) return res.status(400).json({
                 error: true,
