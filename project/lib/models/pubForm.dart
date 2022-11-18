b8dcb57a488ea8fb821853dd1c98e658ff144533
Gustavo Martins <gtavomartins893@gmail.com>
2022-11-17 21:13:20 -0300

    chore: creating send pub form

--- project/lib/models/pubForm.dart
+++ project/lib/models/pubForm.dart
@@ -1,21 +1,21 @@
 class Pub {
   final int id;
   final String vaga;
-  final String descricao;
-  final String horarios;
+  final String desc;
+  final String datasHorarios;
   final String valor;
-  final String modelo_trabalho;
-  final String cidade_bairro;
+  final String modeloTrab;
+  final String cidadeBairro;
   final String imagem; // TODO: como enviar a imagem pro back
 
   const Pub({
     required this.id,
     required this.vaga,
-    required this.descricao,
-    required this.horarios,
+    required this.desc,
+    required this.datasHorarios,
     required this.valor,
-    required this.modelo_trabalho,
-    required this.cidade_bairro,
+    required this.modeloTrab,
+    required this.cidadeBairro,
     required this.imagem,
   });
 
@@ -23,11 +23,11 @@ class Pub {
     return Pub(
       id: json['id'],
       vaga: json['vaga'],
-      descricao: json['descricao'],
-      horarios: json['horarios'],
+      desc: json['desc'],
+      datasHorarios: json['datasHorarios'],
       valor: json['valor'],
-      modelo_trabalho: json['modelo_trabalho'],
-      cidade_bairro: json['cidade_bairro'],
+      modeloTrab: json['modeloTrab'],
+      cidadeBairro: json['cidadeBairro'],
       imagem: json['imagem'],
     );
   }
--- project/lib/screens/subscreens/formPub.dart
+++ project/lib/screens/subscreens/formPub.dart
@@ -1,7 +1,42 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import '../../models/pubForm.dart';
+import 'package:http/http.dart' as http;
+
+Future<Pub> createPub(String vaga, String desc, String datasHorarios,
+    String valor, String modeloTrab, String cidadeBairro, String imagem) async {
+  final response = await http.post(
+    Uri.parse(
+        'https://jsonplaceholder.typicode.com/albums'), //https://localhost:3000/store-Pub/
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      // mas e o id?
+      'vaga': vaga,
+      'desc': desc,
+      'datasHorarios': datasHorarios,
+      'valor': valor,
+      'modeloTrab': modeloTrab,
+      'cidadeBairro': cidadeBairro,
+      'imagem': imagem,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return Pub.fromJson(jsonDecode(response.body)); // estranho (colocar popup?)
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to create Pub.');
+  }
+}
 
 class FormPub extends StatefulWidget {
   const FormPub({super.key});
@@ -11,6 +46,15 @@ class FormPub extends StatefulWidget {
 }
 
 class _FormPubState extends State<FormPub> {
+  
+  final TextEditingController _controllerVaga = TextEditingController();
+  final TextEditingController _controllerDesc = TextEditingController();
+  final TextEditingController _controllerDatasHorarios = TextEditingController();
+  final TextEditingController _controllerValor = TextEditingController();
+  final TextEditingController _controllerModeloTrab = TextEditingController();
+  final TextEditingController _controllerCidadeBairro = TextEditingController();
+  Future<Pub>? _futurePub;
+  
   XFile? image;
   late String vaga;
   late String desc;
@@ -31,6 +75,7 @@ class _FormPubState extends State<FormPub> {
       onSaved: (value) {
         vaga = value.toString();
       },
+      controller: _controllerVaga,
     );
   }
 
@@ -182,7 +227,9 @@ class _FormPubState extends State<FormPub> {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+              children: 
+              // if (_futurePub == null) {
+              <Widget>[
                 buildVaga(),
                 buildDesc(),
                 buildDatasHorarios(),
@@ -197,14 +244,33 @@ class _FormPubState extends State<FormPub> {
                     style: TextStyle(color: Colors.white, fontSize: 14),
                   ),
                   onPressed: () => {
-                    dialogBuilderNotify(context),
+                    // dialogBuilderNotify(context),
+                    setState(() {
+                      _futurePub = createPub(_controllerVaga.text, _controllerDesc, _controllerDatasHorarios,
+    _controllerValor, _controllerModeloTrab, _controllerCidadeBairro/*, imagem*/); // colocar .text em todos
+                    });
                   },
-                )
-              ],
+                ),
+              ],//} else{ buildFutureBuilder() },
             ),
           ),
         ),
       ),
+    );
+  }
+
+  FutureBuilder<Pub> buildFutureBuilder() {
+    return FutureBuilder<Pub>(
+      future: _futurePub,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data!.vaga);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return const CircularProgressIndicator();
+      },
     );
   }
 
