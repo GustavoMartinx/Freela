244c1c1b6c04edecdf92081e3ed67d00033ec2bd
RenanGAS <renansakaoki@gmail.com>
2022-11-20 19:29:53 -0400

    chore: change some attributes

--- Backend/DAOs/usuarioDAO.js
+++ Backend/DAOs/usuarioDAO.js
@@ -6,31 +6,26 @@ import jwtSecret from '../config/auth.js';
 // const bcrypt = require('bcrypt')
 
 class DAOUsuario {
-    async cadastrar(req, res) {
+    async registrar(req, res) {
 
         var dados = req.body;
         // criptografar a senha do usuário antes de guardar no banco de dados
         dados.senha = await bcrypt.hash(dados.senha, 10)
         //cria um usuário e insere no BD
         const user = await Usuario.create(dados, (err) => {
-            if (err) return res.status(400).json({        
+            if (err) return res.status(400).json({
                 error: true,
                 code: 101,
                 message: "Error: Usuário não foi cadastrado com sucesso!"
             });
             // cria um json web token para manter logado
             // é necessário passar esse token sempre q fizer algo 
-           
-            // return res.status(200).json({
-            //     error: false,
-            //     message: "Usuário cadastrado com sucesso!",
-            //     dados: user
-            // });
+
         });
-        const maxAge = 3 * 60 * 60; 
+        const maxAge = 3 * 60 * 60;
         const token = jwt.sign(
-            { id: dados.cpf, nome: dados.nome},
-                jwtSecret,
+            { id: dados.id, nome: dados.nome },
+            jwtSecret,
             {
                 expiresIn: maxAge, // 3hrs in sec
             }
@@ -38,9 +33,9 @@ class DAOUsuario {
         res.cookie("jwt", token, {
             httpOnly: false,
             maxAge: maxAge * 1000, // 3hrs in ms
-            
+
         });
-        res.status(200).json({
+        res.status(201).json({
             error: false,
             message: "Usuário cadastrado com sucesso!",
             dados: user
@@ -50,7 +45,7 @@ class DAOUsuario {
 
     async show(req, res) {
 
-        Usuario.findOne({ where: {id: req.params.id} }).then((user) => {
+        Usuario.findOne({ where: { id: req.params.id } }).then((user) => {
             return res.status(200).json({
                 error: false,
                 user: user
@@ -63,48 +58,53 @@ class DAOUsuario {
             });
         });
     }
-    async login(req, res, next){
-            try {
+    async login(req, res, next) {
+        try {
             // procura o usuário que tentou logar no banco de dados
             var dados = req.body;
-            const user = await Usuario.findOne({ where: {cpf: dados.cpf} })  // achando usuario
+            const user = await Usuario.findOne({ where: { email: dados.email } })  // achando usuario
             //dados.senha = await bcrypt.hash(dados.senha, 10)
             bcrypt.compare(dados.senha, user.senha).then(function (result) {
-            //cria um json web token pra ele
-            // é necessário passar esse token sempre q fizer algo 
-            console.log(result);
+                //cria um json web token pra ele
+                // é necessário passar esse token sempre q fizer algo 
+                console.log(result);
 
-              if (result) {
-                const maxAge = 3 * 60 * 60;
-                const token = jwt.sign(
-                  {id: user.cpf, nome: user.nome},
-                  jwtSecret,
-                  {
-                    expiresIn: maxAge, // 3hrs in sec
-                  }
-                );
-                res.cookie("jwt", token, {
-                  httpOnly: true,
-                  maxAge: maxAge * 1000, // 3hrs in ms
-                });
-                res.status(201).json({
-                  message: "User successfully Logged in"
-                });
-              } else {
-                res.cookie("jwt", 'not today', {
-                    httpOnly: false,
-                    maxAge: 2,
-                  }); // escrevendo reto por linhas tortas AMEM
-                res.status(400).json({ message: "Login not succesful" });
-              }
+                if (result) {
+                    const maxAge = 3 * 60 * 60;
+                    const token = jwt.sign(
+                        { id: user.id, nome: user.nome },
+                        jwtSecret,
+                        {
+                            expiresIn: maxAge, // 3hrs in sec
+                        }
+                    );
+                    res.cookie("jwt", token, {
+                        httpOnly: true,
+                        maxAge: maxAge * 1000, // 3hrs in ms
+                    });
+                    return res.status(201).json({
+                        user: {
+                            id: user._id,
+                            name: user.name,
+                            email: user.email
+                        },
+                        token: token
+                    });
+                } else {
+                    res.cookie("jwt", 'not today', {
+                        httpOnly: false,
+                        maxAge: 2,
+                    }); // escrevendo reto por linhas tortas AMEM
+                    return res.status(400).json({ message: "Login not succesful" });
+                }
             });
-           } catch (error) {
-          res.status(400).json({
-            message: "An error occurred",
-            error: error.message,
-          });
+        } catch (error) {
+            res.status(400).json({
+                message: "An error occurred",
+                error: error.message,
+            });
         }
-        };
+    };
 
     async delete(req, res) {
 
@@ -123,7 +123,7 @@ class DAOUsuario {
             message: "Usuário apagado com sucesso!"
         });
     }
-    async userAuth(req, res, next){
+    async userAuth(req, res, next) {
         const token = req.headers.cookie.slice(4);
         if (token) {
             jwt.verify(token, jwtSecret, (err, decodedToken) => {
@@ -135,9 +135,10 @@ class DAOUsuario {
             })
         } else {
             return res
-            .status(401)
-            .json({ message: "Not authorized, token not available" })
-          }};
+                .status(401)
+                .json({ message: "Not authorized, token not available" })
+        }
+    };
 }
 
 export default new DAOUsuario();
